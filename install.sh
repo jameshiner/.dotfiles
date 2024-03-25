4d15cfb6c233ddb9e485d5e19f543d7fa4f071d4
JJ Hiner <5634000+jameshiner@users.noreply.github.com>
2024-03-24 19:17:30 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,25 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
+# disable spotlight keybinds
+/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist \
+  -c "Delete :AppleSymbolicHotKeys:64" \
+  -c "Add :AppleSymbolicHotKeys:64:enabled bool false" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters array" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 65535" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 49" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 1048576" \
+  -c "Add :AppleSymbolicHotKeys:64:type string standard"
+
+/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist \
+  -c "Delete :AppleSymbolicHotKeys:65" \
+  -c "Add :AppleSymbolicHotKeys:65:enabled bool false" \
+  -c "Add :AppleSymbolicHotKeys:65:value:parameters array" \
+  -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 65535" \
+  -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 49" \
+  -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 1572864" \
+  -c "Add :AppleSymbolicHotKeys:65:type string standard"
+
 # restart dock
 killall Dock
 
